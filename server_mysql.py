c230b46e0add27c1cced1d3cb801cdac2541c3b9
Elizabeth <eemiller4@gmail.com>
2019-07-09 15:31:10 -0700

    adding validation code, flash function

--- server_mysql.py
+++ server_mysql.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from mysqlconnection import connectToMySQL    # import the function that will return an instance of a connection
 app = Flask(__name__)
 @app.route("/")
@@ -8,18 +8,34 @@ def index():
     print(friends)
     return render_template("index.html", all_friends = friends)
 
+# add validation function
 @app.route("/create_friend", methods=["POST"])
-def add_friend_to_db():
+def process():
     print(request.form)
-    mysql = connectToMySQL('first_flask')
-    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (%(fn)s, %(ln)s, %(occ)s, NOW(), NOW());"
-    data = {
-        "fn": request.form['fname'],
-        "ln": request.form['lname'],
-        "occ": request.form['occ']
-    }
-    new_friend_id = mysql.query_db(query,data)
-    return redirect('/')
+    is_valid=True
+    if len(request.form['fname'])<=1:
+        is_valid=False
+        flash("Please enter a first name")
+    if len(request.form['lname'])<=1:
+        is_valid=False
+        flash("Please enter a last name")
+    if len(request.form['occ'])<2:
+        is_valid=False
+        flash("Please enter an occupation, at least 2 characters")
+    if not is_valid:
+        return redirect('/')
+    else:
+        print(request.form)
+        mysql = connectToMySQL('first_flask')
+        query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (%(fn)s, %(ln)s, %(occ)s, NOW(), NOW());"
+        data = {
+            "fn": request.form['fname'],
+            "ln": request.form['lname'],
+            "occ": request.form['occ']
+        }
+        new_friend_id = mysql.query_db(query,data)
+        flash("Insert friend successful!")
+        return redirect('/')
 
             
 if __name__ == "__main__":
