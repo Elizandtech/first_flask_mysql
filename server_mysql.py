d52b651d9f2a87000c7511409187933941db70ad
Elizabeth <eemiller4@gmail.com>
2019-07-09 16:02:07 -0700

    finished flash additions

--- server_mysql.py
+++ server_mysql.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, session, flash
 from mysqlconnection import connectToMySQL    # import the function that will return an instance of a connection
 app = Flask(__name__)
 app.secret_key="Validation"
@@ -15,19 +15,18 @@ def index():
 @app.route("/create_friend", methods=["POST"])
 def process():
     print(request.form)
-    is_valid=True
+    # is_valid=True
     if len(request.form['fname'])<=1:
-        is_valid=False
+        # is_valid=False
         flash("Please enter a first name")
     if len(request.form['lname'])<=1:
-        is_valid=False
+        # is_valid=False
         flash("Please enter a last name")
     if len(request.form['occ'])<2:
-        is_valid=False
+        # is_valid=False
         flash("Please enter an occupation, at least 2 characters")
-    if not is_valid:
-        return redirect('/')
-    else:
+    if not '_flashes' in session.keys():
+
         print(request.form)
         mysql = connectToMySQL('first_flask')
         query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (%(fn)s, %(ln)s, %(occ)s, NOW(), NOW());"
@@ -38,7 +37,7 @@ def process():
         }
         new_friend_id = mysql.query_db(query,data)
         flash("Insert friend successful!")          # flash message: exists for one redirect cycle
-        return redirect('/')
+    return redirect('/')
 
             
 if __name__ == "__main__":
